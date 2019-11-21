2d1fd2e12ca3a57c485596ee01e9ce834ee90090
Boi Soth <naneang415@gmail.com>
2019-11-21 15:27:52 -0800

    Created prepend method for linked list class

--- data-structures/linked-lists.js
+++ data-structures/linked-lists.js
@@ -24,8 +24,15 @@
       this.tail = this.head;
       this.length = 1;
     }
+    prepend(data) {
+      const node = new Node(data);
+      node.next = this.head;
+      this.head = node;
+      this.length++;
+    }
   }
 
   const myList = new LinkedList(5);
+  myList.prepend(2);
   console.log(myList);
 })();
