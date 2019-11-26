06deeca268d1aea9cec976792cbe916e95431044
Boi Soth <naneang415@gmail.com>
2019-11-25 16:31:35 -0800

    Quick notes

--- max-chars/index.js
+++ max-chars/index.js
@@ -7,6 +7,10 @@ function maxChar(str) {
   let max = 0;
   let maxChar = "";
 
+  // Walk through every character of the string
+  // If the character map doesn't have that key(char), assign it a key with the value of 1.
+  // If the character map does already have the key(char), increment the value.
+
   for (let char of str) {
     if (!charMap[char]) {
       charMap[char] = 1;
@@ -14,6 +18,11 @@ function maxChar(str) {
       charMap[char]++;
     }
   }
+
+  // Walk through every key/property of the character object
+  // Check if that current key/property's value is greater than max,
+  // if it is, reassign max to that current keys' value
+  // And also assign the maxChar to that current key
 
   for (let key in charMap) {
     if (charMap[key] > max) {
