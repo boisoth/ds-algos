40500204ed0e051e64dfdfcd798ce2d90b059163
Boi Soth <naneang415@gmail.com>
2019-12-01 19:18:56 -0800

    quick queue array implementation

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -1,9 +1,10 @@
 /**
  * Queue Data Structure
- * enqueue
- * dequeue
- * isEmpty
- * printQueue
+ * enqueue - add to the end
+ * dequeue - remove from the front
+ * isEmpty - returns true if no length
+ * printQueue - if queue is not empty, return queue as an array
+ *
  * Implementation
  * Array enqueue O(1), add to head O(n)
  * Linked Lists O(1), add to head O(1)
@@ -21,7 +22,23 @@ class QueueA {
   constructor() {
     this.data = [];
   }
+  printQueue() {
+    if (!this.data.length) {
+      return "Queue is empty";
+    }
+    return this.data;
+  }
+  enqueue(data) {
+    this.data.push(data);
+    return this.printQueue();
+  }
 }
+
+const myQueue = new QueueA();
+myQueue.enqueue(20);
+myQueue.enqueue(30);
+myQueue.enqueue(40);
+console.log(myQueue.printQueue());
 
 // Queue Linked List Implementation
 class QueueL {}
