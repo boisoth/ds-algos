63e2ef91072078440a79e9fd7c3c894a2639a712
Boi Soth <naneang415@gmail.com>
2019-12-01 17:08:38 -0800

    setting up queue as array

---
+++ data-structures/queue.js
@@ -0,0 +1,24 @@
+/**
+ * Queue Data Structure
+ * enqueue
+ * dequeue
+ * isEmpty
+ * printQueue
+ * Implementation
+ * Array enqueue O(1), add to head O(n)
+ * Linked Lists O(1), add to head O(1)
+ */
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// Queue Array Implementation
+class QueueA {
+  constructor() {
+    this.data = [];
+  }
+}
