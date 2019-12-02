fa6eccdb063ac29f32fc41e66033fb5e58b6ef05
Boi Soth <naneang415@gmail.com>
2019-12-01 16:38:54 -0800

    adding a few methods

--- data-structures/stacks.js
+++ data-structures/stacks.js
@@ -1,3 +1,29 @@
 /**
  * Stacks
+ * push
+ * pop -- return and remove last element
+ * peek -- return last element
+ * isEmpty -- return if no length
+ * printStack -- print all items in stack
  */
+
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+  push(item) {
+    this.items.push(item);
+  }
+  pop() {
+    if (!this.items.length) {
+      return "No items";
+    }
+    return this.items.pop();
+  }
+  peek() {
+    if (!this.items.length) {
+      return "No items";
+    }
+    return this.items.length - 1;
+  }
+}
